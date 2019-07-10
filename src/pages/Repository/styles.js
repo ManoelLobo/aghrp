8bf4dc05a8bed463bc057bdef637e2536399776a
Manoel <manoel.lobo@gmail.com>
2019-07-10 14:32:19 -0300

    list repository issues (5 last, open)

--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -30,7 +30,7 @@ export const Owner = styled.header`
   p {
     margin-top: 5px;
     font-size: 14px;
-    color: #809ab0;
+    color: #506688;
     line-height: 1.4;
     text-align: center;
     max-width: 420px;
@@ -40,5 +40,65 @@ export const Owner = styled.header`
     color: #3ebd93;
     font-size: 16px;
     text-decoration: none;
+  }
+`;
+
+export const IssueList = styled.ul`
+  padding-top: 30px;
+  margin-top: 30px;
+  border-top: 1px solid #9fb3c8;
+  list-style: none;
+
+  li {
+    display: flex;
+    padding: 15px 10px;
+    border: 1px solid #9fb3c8;
+    border-radius: 4px;
+
+    & + li {
+      margin-top: 10px;
+    }
+
+    img {
+      width: 36px;
+      height: 36px;
+      border-radius: 50%;
+      border: 2px solid #9fb3c8;
+    }
+
+    div {
+      flex: 1;
+      margin-left: 15px;
+
+      strong {
+        font-size: 16px;
+
+        a {
+          text-decoration: none;
+          color: #102b44;
+
+          &:hover {
+            color: #3ebd93;
+          }
+        }
+
+        span {
+          background: #9fb3c8;
+          color: #102b44;
+          border-radius: 2px;
+          font-size: 12px;
+          font-weight: 600;
+          height: 20px;
+          padding: 3px 4px;
+          margin-left: 10px;
+        }
+      }
+
+      p {
+        margin-top: 5px;
+        font-size: 12px;
+        color: #809ab0;
+      }
+    }
   }
 `;
